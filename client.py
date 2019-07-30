631e135671dd91f0e1ba4176b509d0e47f226353
matiasnferrero <matiasnferrero@gmail.com>
2019-07-30 20:35:36 +0200

    added flexibility

--- client.py
+++ client.py
@@ -4,11 +4,19 @@ import socket
 
 s = socket.socket()
 
-hostname = "matias-Precision-M6700"
+### check in which host is running the client
+hostname_client = socket.gethostname()
+
+if hostname_client == "matias-TUF-Gaming-FX705GM-FX705GM":
+    hostname_server = "matias-Precision-M6700"
+
+else:
+    hostname_server = "matias-TUF-Gaming-FX705GM-FX705GM"
+
 
 port = 8000
 
-s.connect( ( hostname , port  ))
+s.connect( ( hostname_server , port  ))
 
 running = True
 
