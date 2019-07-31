8a48320bc4eddc04d5de2052637a93cf525cbddd
Matias <matiasnferrero@gmail.com>
2019-07-31 17:45:53 +0200

    changed server input

--- client.py
+++ client.py
@@ -4,19 +4,13 @@ import socket
 
 s = socket.socket()
 
-### check in which host is running the client
-hostname_client = socket.gethostname()
 
-if hostname_client == "matias-TUF-Gaming-FX705GM-FX705GM":
-    hostname_server = "matias-Precision-M6700"
-
-else:
-    hostname_server = "matias-TUF-Gaming-FX705GM-FX705GM"
+server_ip = input("Please write the server IP")
 
 
 port = 8000
 
-s.connect( ( hostname_server , port  ))
+s.connect( ( server_ip , port  ))
 
 running = True
 
